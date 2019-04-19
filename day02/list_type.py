b100db7efa835f19822169d666bacd61bc001ef5
闫松林 <songlinlin1994@163.com>
2019-04-19 15:41:58 +0800

    新增 list 增加元素

--- day02/list_type.py
+++ day02/list_type.py
@@ -15,16 +15,34 @@ def list_sel(a):
     print(a[3:])
 
 def list_del():
+    # 定义一个list
     alist = ['a', 4, 'nihao', '8', '就是', '哈']
+    # 调用删除方法 不填参数 默认删除最后一位
     alist.pop()
     print(alist)
+    # 调用删除方法, 填写参数: 索引值   就可以删除指定元素
     alist.pop(0)
     print(alist)
 
+    # 将切片获取的 元素 重新定义一个list 保存起来
+    blist = alist[:-3]
+    print(blist)
+
+def list_add():
+    alist = ['a', 4, 4, 4, 'nihao', '8', '就是', '哈']
+    # 增加元素 ,增加在末尾
+    alist.append('哈哈哈哈')
+    print(alist)
+
+    blist = [1,2,3]
+    # 将一个list 作为元素,添加到 list里面
+    alist.append(blist)
+    print(alist)
 
 
 if __name__ == '__main__':
     alist = ['a',4,'nihao','8','就是','哈']
     # list_sel(alist)
     # print(alist)
-    list_del()
+    # list_del()
+    list_add()
